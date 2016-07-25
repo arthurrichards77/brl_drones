56506cca019ba74e6c0f9dfe753f9f480ffd679b
Arthur <arthur.richards@bristol.ac.uk>
2016-07-25 16:02:16 +0100

    Arthur added external mode to control panel

--- scripts/pid_virtual_drone_test.py
+++ scripts/pid_virtual_drone_test.py
@@ -6,7 +6,7 @@ import brl_drones.rospid
 
 rospy.init_node('testpid')
 # control drone2 to follow drone 1
-quad_pid = brl_drones.rospid.Quadpid('drone2_hull','drone1_hull',ctrl_topic='/drone2/cmd_vel')
+quad_pid = brl_drones.rospid.Quadpid('drone2_hull','drone1_hull',ctrl_topic='/drone2/ext_vel')
 
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
